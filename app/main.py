c3e24bd3dc8fd170a81b2945c0d74d0e93b778c9
Abinavv1 <abhinavsharma.sh@gmail.com>
2025-09-24 20:46:25 +0545

    feat: added cors middleware

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 
+from app.core.settings import get_settings
 from app.core.events import lifespan
 from app.core.exceptions.handler import add_exception_handlers
+from fastapi.middleware.cors import CORSMiddleware
 from app.endpoints import router as api_router
+
+settings = get_settings()
 
 app = FastAPI(
     title="Restaurant billing management system.",
@@ -11,3 +15,16 @@ app = FastAPI(
 
 app.include_router(router=api_router)
 add_exception_handlers(app=app)
+
+origins = [
+    "http://192.168.10.103:3000",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
