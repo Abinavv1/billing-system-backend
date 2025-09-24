54848134aa50b1984526af1781e962b82484ccce
Abinavv1 <abhinavsharma.sh@gmail.com>
2025-09-24 20:42:28 +0545

    feat: added order router

--- app/endpoints/__init__.py
+++ app/endpoints/__init__.py
@@ -3,9 +3,11 @@ from fastapi import APIRouter
 from .auth import router as auth_router
 from .user import router as user_router
 from .menu_item import router as menu_item_router
+from .order import router as order_router
 
 router = APIRouter(prefix='/api')
 router.include_router(router=auth_router)
 router.include_router(router=user_router)
 router.include_router(router=menu_item_router)
+router.include_router(router=order_router)
 
