b86f7564d78740835e61da196433ba102b647cc6
Abinavv1 <abhinavsharma.sh@gmail.com>
2025-09-25 17:41:07 +0545

    feat: added payment status enum

--- app/core/enums/payment.py
+++ app/core/enums/payment.py
@@ -3,3 +3,8 @@ from enum import Enum
 class PaymentType(Enum):
     cash = "cash"
     esewa = "esewa"
+
+class PaymentStatus(Enum):
+    PENDING = "pending"
+    COMPLETED = "completed"
+    CANCELLED = "cancelled"
